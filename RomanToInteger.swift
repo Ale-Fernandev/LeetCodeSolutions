28fe58de5489e0cf377f0a95795bfec6d9848f14
Ale-Fernandev <157256517+Ale-Fernandev@users.noreply.github.com>
2024-04-15 16:03:55 -0400

    Publishing {WIP}
    WIP --

---
+++ RomanToInteger.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+func romanToInt(_ roman: String) -> Int {
+    //for character in roman {
+        //for value in key {
+            //if character == X or M,C,B all characters for cases
+    // check for the one after
+    // skip iteration of the next
+    // if its possible to equal the value ( not nil ) do it and add previous value += to sum
+    //
+       // }
+   // }
+    return 0
+}
+
+
